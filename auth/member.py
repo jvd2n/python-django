615bc2f887a81e8af171caddb452c26cfb279c19
muten-dev <juhnmayer@gmail.com>
2021-05-25 22:22:38 +0900

    auth

--- auth/member.py
+++ auth/member.py
@@ -1,51 +1,73 @@
 class Member(object):
-    m_id = ''
-    pwd = ''
-    email = ''
-    phone = ''
+    # m_id = ''
+    # pwd = ''
+    # email = ''
+    mem_list = []
 
-    # def __init__(self, m_id, pwd, email, phone):
-    #     self.m_id = m_id
-    #     self.pwd = pwd
-    #     self.email = email
-    #     self.phone = phone
+    def __init__(self, m_id=None, pwd=None, email=None):
+        if m_id is None and pwd is None and email is None:
+            print('Default Constructor')
+        else:
+            self.m_id = m_id
+            self.pwd = pwd
+            self.email = email
+            print('__init__ Constructor')
 
-    def join(self, m_id, pwd, email, phone):
-        self.m_id = m_id
-        self.pwd = pwd
-        self.email = email
-        self.phone = phone
+    def __str__(self):
+        return f'{self.m_id}({self.email})'
+
+    def join(self, m_id, pwd, email):
+        is_exist = False
+        for i in self.mem_list:
+            if i.m_id == m_id:
+                is_exist = True
+        if is_exist:
+            print(f'ERROR : {m_id} is N/A')
+        else:
+            self.mem_list.append(Member(m_id, pwd, email))
+            print(f'SUCCESS : {m_id} JOINED')
 
     def login(self, m_id, pwd):
-        if self.m_id == m_id and self.pwd == pwd:
-            print(f'{m_id}님, 환영합니다.')
-        else:
-            print(f'잘못된 입력입니다.')
-
-    def change_pwd(self, orig_pwd, new_pwd):
-        if self.pwd == orig_pwd:
-            self.pwd = new_pwd
-        else:
+        is_exist = False
+        for i in self.mem_list:
+            if i.m_id != m_id:
+                continue
+            else:
+                is_exist = True
+                if i.pwd == pwd:
+                    print(f'Welcome, {m_id}')
+                else:
+                    print('Wrong Password')
+        if is_exist:
             pass
+        else:
+            print('There is no ID')
 
-    def to_string(self):
-        return f'{self.m_id}, {self.email}, {self.phone}'
+    def member_info(self):
+        for i, j in enumerate(self.mem_list):
+            print(f'|{i}| {j}')
 
     @staticmethod
     def main():
         member = Member()
+        mem_ls = []
         while 1:
-            menu = input('1 JOIN 2 LOGIN 3 MYPAGE '
+            menu = input('1 JOIN 2 LOGIN 3 MEMBER_INFO\n'
                          '4 UPDATE 5 REMOVE\n0 EXIT\n>> ')
             if menu == '1':
-                member.join(input('id? '), input('pwd? '),
-                            input('email? '), input('phone? '))
+                member.join(input('id? '), input('pwd? '), input('email? '))
             elif menu == '2':
                 member.login(input('your id : '), input('your pw : '))
             elif menu == '3':
-                print(member.to_string())
+                member.member_info()
+            elif menu == '4':
+                pass
+            elif menu == '5':
+                pass
             elif menu == '0':
                 break
+            else:
+                print('Wrong Menu NO.')
 
 
 Member.main()
